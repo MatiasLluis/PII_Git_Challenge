e8c878fbda3d6b30c28721cbf209a3b2cb684071
Facundo1105 <FACUNDO.ROSALES@CORREO.UCU.EDU.UY>
2025-03-19 19:32:05 -0300

    Arreglos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,3 +1,9 @@
+Console.WriteLine(Addition.Add(7, 2));
+Console.WriteLine(Substraction.Substract(10, 1));
+Console.WriteLine(Multiplication.Multiply(3, 3));
+Console.WriteLine(Division.Divide(18, 2));
+
+// Esta clse implementa la operacion suma
 public class Multiplication
 {
     public static int Multiply(int a, int b)
@@ -5,11 +11,6 @@ public class Multiplication
         return 0;
     }
 }
-Console.WriteLine(Addition.Add(7, 2));
-Console.WriteLine(Substraction.Substract(10, 1));
-Console.WriteLine(Multiplication.Multiply(3, 3));
-Console.WriteLine(Division.Divide(18, 2));
-
 
 //Esta clase implementa la operacion suma
 public class Addition
